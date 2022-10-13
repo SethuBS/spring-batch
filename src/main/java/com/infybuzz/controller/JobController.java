572e3d34d1581f348af1aa7d8b99e4b4eaff7c72
Sethu Budaza <sethu.budaza@payu.com>
2022-10-13 10:28:04 +0200

    Stop job using rest api

--- src/main/java/com/infybuzz/controller/JobController.java
+++ src/main/java/com/infybuzz/controller/JobController.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -22,10 +23,19 @@ public class JobController {
     @Autowired
     private JobService jobService;
 
+    @Autowired
+    private JobOperator jobOperator;
+
 
     @GetMapping("/start/{jobName}")
     public String startJob(@PathVariable String jobName, @RequestBody List<JobParamsRequest> jobParamsRequestList) throws Exception{
         jobService.startJob(jobName,jobParamsRequestList);
         return "Job started.......";
+    }
+
+    @GetMapping("/stop/{executionId}")
+    public String stopJob(@PathVariable long executionId) throws Exception{
+        jobOperator.stop(executionId);
+        return "Jop Stopped.......";
     }
 }
